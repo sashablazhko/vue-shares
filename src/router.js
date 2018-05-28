80c7c1e7eb97340f53262d930dcaab8e65a73098
sashablazhko <sashablazhko@gmail.com>
2018-05-28 11:08:58 +0300

    first commit

--- src/router.js
+++ src/router.js
@@ -1,21 +1,26 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+
+import Home from "./components/Home.vue";
+import Portfolio from "./components/portfolio/Portfolio.vue";
+import Stocks from "./components/stocks/Stocks.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: About
+      path: "/portfolio",
+      component: Portfolio
+    },
+    {
+      path: "/stocks",
+      component: Stocks
     }
   ]
 });
